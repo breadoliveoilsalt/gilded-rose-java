0b7588162ad557d1f3caf177e671b7e3a2cac18a
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-18 16:35:58 -0500

    Initial commit with .gitignore, .travis.yml, gradle.build, and sample test

---
+++ src/test/java/gildedRoseTests/SampleTest.java
@@ -0,0 +1,13 @@
+package gildedRoseTests;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SampleTest {
+
+    @Test
+    public void testSampleTestPasses() {
+        assertTrue(true);
+    }
+}
