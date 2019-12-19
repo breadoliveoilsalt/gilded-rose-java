5d065635f0c5d81d0f6238422ac335c1c0d50103
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-19 16:12:08 -0500

    Refactor to isolate Backstage Pass logic in separate conditional

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -26,41 +26,30 @@ public class GildedRose {
                 continue;
             }
 
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    items[i].quality = items[i].quality - 1;
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn > 10) {
+                    item.quality = Math.min(item.quality + 1, 50);
+                } else if (item.sellIn > 5) {
+                    item.quality = Math.min(item.quality + 2, 50);
+                } else if (item.sellIn > 0) {
+                    item.quality = Math.min(item.quality + 3, 50);
+                } else {
+                    item.quality = 0;
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                items[i].sellIn = items[i].sellIn - 1;
+                continue;
+            }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+            if (items[i].quality > 0) {
+                items[i].quality = items[i].quality - 1;
             }
 
             items[i].sellIn = items[i].sellIn - 1;
 
             if (items[i].sellIn < 0) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
+                if (items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1;
+                }
             }
         }
     }
