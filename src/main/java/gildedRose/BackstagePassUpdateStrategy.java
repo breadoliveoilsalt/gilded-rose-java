2b72eef2decb05f3ef5cef73314bcd6b287d9995
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-27 15:44:14 -0500

    Add BackstagePassUpdateStrategy

---
+++ src/main/java/gildedRose/BackstagePassUpdateStrategy.java
@@ -0,0 +1,17 @@
+package gildedRose;
+
+public class BackstagePassUpdateStrategy implements ItemUpdateStrategy {
+
+    public void update(Item item) {
+        if (item.sellIn > 10) {
+            item.quality = Math.min(item.quality + 1, 50);
+        } else if (item.sellIn > 5) {
+            item.quality = Math.min(item.quality + 2, 50);
+        } else if (item.sellIn > 0) {
+            item.quality = Math.min(item.quality + 3, 50);
+        } else {
+            item.quality = 0;
+        }
+        item.sellIn = item.sellIn - 1;
+    }
+}
