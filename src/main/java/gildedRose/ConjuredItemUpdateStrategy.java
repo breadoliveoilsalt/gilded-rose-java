aafa1f3c6238bc2b2a2bef0d507959f17f877ec9
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-27 15:46:39 -0500

    Add ConjuredItemUpdateStrategy

---
+++ src/main/java/gildedRose/ConjuredItemUpdateStrategy.java
@@ -0,0 +1,14 @@
+package gildedRose;
+
+public class ConjuredItemUpdateStrategy implements ItemUpdateStrategy {
+
+    public void update(Item item) {
+        if (item.sellIn > 0) {
+            item.quality = Math.max(item.quality - 2, 0);
+        } else {
+            item.quality = Math.max(item.quality - 4, 0);
+        }
+        item.sellIn = item.sellIn - 1;
+    }
+
+}
