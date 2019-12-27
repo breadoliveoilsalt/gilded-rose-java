b1f21b397a415ce17fe6864262735c0f04213351
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-27 15:53:08 -0500

    Add setUpdateStrategy() and update() to Item

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -5,11 +5,16 @@ public class Item {
     public String name;
     public int sellIn;
     public int quality;
+    private ItemUpdateStrategy updateStrategy;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+    }
+
+    public void setUpdateStrategy(ItemUpdateStrategy updateStrategy) {
+        this.updateStrategy = updateStrategy;
     }
 
 }
