fe01995b92484f4b54c72c8f138ba1fd53c37e1d
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-27 15:50:06 -0500

    Add DefaultItemUpdateStrategy

---
+++ src/main/java/gildedRose/DefaultItemUpdateStrategy.java
@@ -0,0 +1,14 @@
+package gildedRose;
+
+public class DefaultItemUpdateStrategy implements ItemUpdateStrategy {
+
+    public void update(Item item) {
+        if (item.sellIn > 0) {
+            item.quality = Math.max(item.quality - 1, 0);
+        } else {
+            item.quality = Math.max(item.quality - 2, 0);
+        }
+        item.sellIn = item.sellIn - 1;
+    }
+
+}
